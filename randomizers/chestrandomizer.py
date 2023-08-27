c3d4b845375030fd7c46165ed06239703ba97708
Nhan Nguyen <nnguyen259@outlook.com>
2023-08-26 21:18:36 -0400

    Chest Randomizer Rebalance

--- randomizers/chestrandomizer.py
+++ randomizers/chestrandomizer.py
@@ -15,37 +15,15 @@ from libcst._removal_sentinel import RemovalSentinel
 
 # fmt: off
 master_quartz = [3607, 3598, 3593, 3599, 3594, 3600, 3606, 3614, 3605, 3640, 3613, 3624, 3603, 3592]
-single_chest_options = [3748, 3749, 3750, 3751, 3752, 3753, 3754, 3755,
-                        3820, 3821, 3822, 3823, 3824, 3825, 3826, 3827, 3828, 3829,
-                        3887, 3888, 3889, 3890, 3891, 3892, 3893, 3894,
-                        3951, 3952, 3953, 3954, 3955, 3956, 3957,
-                        4007, 4008, 4009, 4010, 4011, 4012, 
-                        4062, 4063, 4064, 4065, 4066, 4067,
-                        4118, 4119, 4120, 4121, 4122, 4123,
-                        3760, 3759, 3761, 3762, 3763, 3764, 3765,
-                        3834, 3833, 3835, 3837, 3836, 3838,
-                        3899, 3898, 3900, 3901, 3902, 3903, 3904, 3905,
-                        3962, 3961, 3964, 3963, 3965, 3966, 3967,
-                        4016, 4015, 4017, 4018, 4019, 4020, 4021,
-                        4070, 4071, 4069, 4072, 4073, 4074, 4075, 4076,
-                        4132, 4126, 4127, 4128, 4130, 4129, 4131,
-                        302, 15, 16, 17, 18, 20, 21, 61, 62, 63,
-                        2612, 2613, 2616, 2617, 2620, 2621, 2624, 2625, 2628, 2629, 2632, 2633, 2636, 2637,
-                        2640, 2641, 2644, 2645, 2648, 2649, 2652, 2653, 2656, 2657, 2660, 2661, 2664, 2665,
-                        2668, 2669, 2672, 2673, 2676, 2677, 2680, 2681, 2684, 2685, 2686, 2689, 2690, 2691,
-                        2692, 2693, 2694, 2716, 2731, 2740, 2741, 2742, 2743, 2744, 2745, 2746, 2747]
 
 multi_chest_options = [(300, 300), (310, 3000), (311, 3000), (312, 3000), (313, 3000), (314, 3000), (315, 3000), (316, 3000),
-                       (300, 300), (310, 3000), (311, 3000), (312, 3000), (313, 3000), (314, 3000), (315, 3000), (316, 3000),
-                       (300, 300), (310, 3000), (311, 3000), (312, 3000), (313, 3000), (314, 3000), (315, 3000), (316, 3000),
-                       (3, 10), (8, 10), (11, 5), (37, 5), (50, 10), (51, 10), (52, 10), (53, 10), (54, 10), (58, 3), (59, 3), (60, 3),
-                       (3737, 2), (3738, 2), (3756, 2), (3757, 2), (3758, 2),
-                       (3809, 2), (3810, 2), (3830, 2), (3831, 2), (3832, 2),
-                       (3876, 2), (3877, 2), (3895, 2), (3896, 2), (3897, 2),
-                       (3939, 2), (3940, 2), (3958, 2), (3959, 2), (3960, 2),
-                       (3997, 2), (3998, 2), (4013, 2), (4014, 2),
-                       (4051, 2), (4052, 2), (4054, 2), (4068, 2),
-                       (4107, 2), (4108, 2), (4124, 2), (4125, 2)]
+                       (303, 200), (50, 40), (51, 40), (52, 40), (53, 40), (54, 40)]
+
+chest_chances = [[40, 35, 10, 5, 5, 5],
+                 [20, 40, 20, 10, 5, 5],
+                 [0, 20, 40, 20, 15, 5],
+                 [0, 0, 10, 50, 35, 5],
+                 [0, 0, 0, 30, 65, 5]]
 
 
 # fmt: on
@@ -157,9 +135,17 @@ def randomize_chest(seed: str, directory: Path):
     local_random = random.Random(seed)
     local_random.shuffle(master_quartz)
     processes = list()
+    single_items = list()
 
     with open("setup/chests.json") as chestfile:
         chests = json.load(chestfile)
+
+    with open("setup/items.json", encoding="utf8") as itemfile:
+        item_ids = json.load(itemfile)
+
+    for i in range(1, 7):
+        with open(f"setup/tier{i}_items.json", encoding="utf8") as itemfile:
+            single_items.append(json.load(itemfile))
 
     current_dir = Path.cwd()
     shutil.rmtree(f"tmp/{seed}", ignore_errors=True)
@@ -169,9 +155,16 @@ def randomize_chest(seed: str, directory: Path):
     os.chdir(f"tmp/{seed}")
 
     for i, act in enumerate(chests):
-        maps, single_chests, _ = list(zip(*act))
+        maps, single_chests, _, chest_tier = list(zip(*act))
         total_chests = sum(single_chests)
-        chest_pool = random.choices(single_chest_options, k=total_chests - 1)
+        chest_tier = chest_tier[0]
+        chest_rate = chest_chances[chest_tier - 1]
+        chest_pool = list()
+        tiers = local_random.choices([0, 1, 2, 3, 4, 5], weights=chest_rate, k=total_chests - 1)
+        for tier in tiers:
+            item_pool = single_items[tier]
+            category = local_random.choices(list(item_pool["categories"].keys()), weights=item_pool["odds"], k=1)[0]
+            chest_pool.append(item_ids[local_random.choice(item_pool["categories"][category])])
         chest_pool.append(master_quartz.pop())
         local_random.shuffle(chest_pool)
         visitor = ChestVisitor(chest_pool, multi_chest_options, random.Random(f"{seed}_{i}"))
